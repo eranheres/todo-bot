076ce348ba76d23b5e756f5a9611738684e1dae9
EranH <eran.heres@gmail.com>
2021-03-22 10:45:04 +0200

    connect

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from todobot.slack_listener import start_listen
+from flask import escape
 
 
-def todobot():
-    start_listen()
+def todobot(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
 
 
 if __name__ == '__main__':
-    todobot()
+    pass
